093b2b9358e199fbc422770999d9861899a7a319
joaccocaballero <joaquincaballero182@gmail.com>
2022-12-21 19:57:47 -0300

    .

--- middlewares/tokenValidation.js
+++ middlewares/tokenValidation.js
@@ -2,7 +2,7 @@ require('dotenv').config()
 const jwt = require('jsonwebtoken')
 
 const verifyToken = (req, res, next) => {
-    const token = req.header('Authorization');
+    const token = req.header('Authentication');
     try {
         const verified = jwt.verify(token, process.env.TOKEN_SECRET)
         req.user = verified
